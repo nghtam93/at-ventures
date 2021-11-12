2e758c44cfd8f9daf1cf3097f5e783177309b6da
doannguyen263 <doannguyen263@gmail.com>
2021-11-12 08:07:27 +0700

    Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 
-
-
     var header_sticky=$("header.-fix")
     if($('body').hasClass( "home" )){
         $(window).scroll(function(){
@@ -10,6 +8,58 @@ $(document).ready(function(){
     }else{
         header_sticky.addClass("is-active")
     }
+
+    //-------------------------------------------------
+    // Header Search
+    //-------------------------------------------------
+    var $headerSearch = $('.header__search');
+    var $headerSearchToggle = $('.search-submit');
+
+
+    $headerSearchToggle.click(function(e){
+        if($(".search-field").val().length == 0){
+          e.preventDefault();
+          e.stopPropagation()
+        }
+    })
+
+    $headerSearchToggle.on('click', function(e) {
+
+        var $this = $(this);
+        var $header = $(this).closest('header');
+        var $this_parent = $(this).closest('form');
+        if(!$header.hasClass('open-search')) {
+            $header.addClass('open-search');
+
+        } else {
+            $header.removeClass('open-search');
+        }
+    });
+
+
+
+
+
+    // $('#search').on("click",(function(e){
+    //   $(".form-group").addClass("sb-search-open");
+    //     e.stopPropagation()
+    // }));
+
+    // $(document).on("click", function(e) {
+    //     if ($(e.target).is("#search") === false && $(".form-control").val().length == 0) {
+    //       $(".form-group").removeClass("sb-search-open");
+    //     }
+    // });
+
+    // $(".form-control-submit").click(function(e){
+    //       $(".form-control").each(function(){
+    //         if($(".form-control").val().length == 0){
+    //           e.preventDefault();
+    //           $(this).css('border', '2px solid red');
+    //         }
+    //     })
+    // })
+
 
     //-------------------------------------------------
     // Menu
@@ -25,17 +75,25 @@ $(document).ready(function(){
         $('.nav__mobile').addClass('active')
     });
 
-    var back_to_top=$(".scrollTop"),offset=220,duration=500;$(window).scroll(function(){$(this).scrollTop()>offset?back_to_top.addClass("active"):back_to_top.removeClass("active")}),$(document).on("click",".scrollTop",function(o){return o.preventDefault(),$("html, body").animate({scrollTop:0},duration),!1});
+    var back_to_top=$(".back-to-top"),offset=220,duration=500;
+
+    $(document).on("click",".back-to-top",function(o){
+        return o.preventDefault(),$("html, body").animate({scrollTop:0},duration),!1
+    });
 
     //check home
     if($('body').hasClass( "home" )){
 
         new WOW().init();
-        $('.home-news-slider').slick({
-            slidesToShow: 3,
+        $('.home-solution-slider').slick({
+            slidesToShow: 1,
             slidesToScroll: 1,
-            dots: false,
-            easing: "linear"
+            dots: true,
+            easing: "linear",
+            centerMode: true,
+            variableWidth: true,
+            prevArrow: '<span class="icon-arrow-left slick-prev slick-arrow"></span>',
+            nextArrow: '<span class="icon-arrow-right slick-next slick-arrow"></span>'
         });
     }
 
