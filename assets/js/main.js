4fc56708a9d309bc586b3ea0206f43589b03d96c
doannguyen263 <doannguyen263@gmail.com>
2021-11-16 09:08:14 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,35 +108,33 @@ $(document).ready(function(){
         });
     }
 
-    if($('body').hasClass( "about" )){
-        $('.about-slider').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            dots: true,
-            easing: "linear",
-            prevArrow: '<span class="icon-arrow-left slick-prev slick-arrow"></span>',
-            nextArrow: '<span class="icon-arrow-right slick-next slick-arrow"></span>',
-            autoplay: true,
-            autoplaySpeed: 2000,
-            responsive: [
-                {
-                  breakpoint: 991,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                  }
-                },
-                {
-                  breakpoint: 575,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                  }
-                }
-            ]
-        });
 
-    }
+    $('.about-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        easing: "linear",
+        prevArrow: '<span class="icon-arrow-left slick-prev slick-arrow"></span>',
+        nextArrow: '<span class="icon-arrow-right slick-next slick-arrow"></span>',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+        ]
+    });
 
 });
 
